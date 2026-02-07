18af6266be5915e8f675d6d900ce91ba1aa01fbe
Your Name <you@example.com>
2026-02-07 03:17:50 +0300

    готовый

--- src/org/skypro/skyshop/App.java
+++ src/org/skypro/skyshop/App.java
@@ -1,51 +1,41 @@
 package org.skypro.skyshop;
 
 import org.skypro.skyshop.product.Product;
+import org.skypro.skyshop.product.SimpleProduct;
+import org.skypro.skyshop.product.DiscountedProduct;
+import org.skypro.skyshop.product.FixPriceProduct;
 import org.skypro.skyshop.basket.ProductBasket;
 
-class App {
+public class App {
     public static void main(String[] args) {
-        Product milk = new Product("Молоко", 50);
-        Product bread = new Product("Хлеб", 30);
-        Product cheese = new Product("Сыр", 120);
-        Product tea = new Product("Чай", 80);
-        Product coffee = new Product("Кофе", 150);
-        Product extra = new Product("Шоколад", 70);
+        SimpleProduct milk = new SimpleProduct("Молоко", 50);
+        SimpleProduct bread = new SimpleProduct("Хлеб", 30);
+        DiscountedProduct cheese = new DiscountedProduct("Сыр", 120, 25);
+        DiscountedProduct tea = new DiscountedProduct("Чай", 80, 10);
+        FixPriceProduct coffee = new FixPriceProduct("Кофе");
+        FixPriceProduct water = new FixPriceProduct("Вода");
 
         ProductBasket basket = new ProductBasket();
 
-        // 1. Добавление продуктов в корзину
         basket.addProduct(milk);
         basket.addProduct(bread);
         basket.addProduct(cheese);
         basket.addProduct(tea);
         basket.addProduct(coffee);
 
-        // 2. Попытка добавить продукт в заполненную корзину
-        basket.addProduct(extra);
+        basket.addProduct(water);
 
-        // 3. Печать содержимого корзины
         basket.printBasket();
 
-        // 4. Получение стоимости корзины
         System.out.println("Стоимость корзины: " + basket.getTotalPrice());
-
-        // 5. Поиск товара, который есть в корзине
         System.out.println("Есть ли 'Хлеб' в корзине? " + basket.hasProductByName("Хлеб"));
-
-        // 6. Поиск товара, которого нет в корзине
         System.out.println("Есть ли 'Яблоко' в корзине? " + basket.hasProductByName("Яблоко"));
 
-        // 7. Очистка корзины
         basket.clear();
 
-        // 8. Печать содержимого пустой корзины
         basket.printBasket();
 
-        // 9. Получение стоимости пустой корзины
         System.out.println("Стоимость пустой корзины: " + basket.getTotalPrice());
-
-        // 10. Поиск товара по имени в пустой корзине
         System.out.println("Есть ли 'Молоко' в пустой корзине? " + basket.hasProductByName("Молоко"));
     }
 }
